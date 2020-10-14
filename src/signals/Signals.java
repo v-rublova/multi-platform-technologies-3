b9de800d48f6f6c7f0ded7defee47cb31aa336d5
v.rublova <v.rublova@student.csn.khai.edu>
2020-10-14 19:38:41 +0300

    Initial

---
+++ src/signals/Signals.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package signals;
+
+import Calc.Signal_calculations;
+import filework.Get_file;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author v.rublova
+ */
+public class Signals {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        Signal_calculations obj = new Signal_calculations();
+        Get_file file_w = new Get_file();
+        if (args.length < 1) {
+            System.out.println("Please provide input and output files");
+            System.exit(0);
+        } else {
+            if (Get_file.IfFilesAreGood(args)) {
+                Integer byteRead;
+                ArrayList<Integer> data = new ArrayList<Integer>();
+                while ((byteRead = file_w.fin.read()) != -1) {
+                   data.add(byteRead);
+                }
+            } else {
+                System.out.println("Some files are undetected.");
+                System.exit(0);
+            }
+        }
+    }
+
+}
